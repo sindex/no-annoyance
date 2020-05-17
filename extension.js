866084ec8c134e56fd58e9454453e08734170e46
Alex Papachristou <sindex@users.noreply.github.com>
2020-05-17 13:42:06 +0300

    Rewrite

--- extension.js
+++ extension.js
@@ -1,57 +1,45 @@
 const Main = imports.ui.main;
-const WindowAttentionHandler = imports.ui.windowAttentionHandler;
-const Shell = imports.gi.Shell;
-const Lang = imports.lang;
 
-function StealMyFocus() {
-  this._init();
+function DontAnnoy() {
+    this._init();
 }
 
-function WindowIsReadyRemover() {
-  this._init();
+DontAnnoy.prototype = {
+    _init : function() {
+        global.display.disconnect(Main.windowAttentionHandler._windowDemandsAttentionId);
+        global.display.disconnect(Main.windowAttentionHandler._windowMarkedUrgentId);
+        this._windowDemandsAttentionId = global.display.connect('window-demands-attention',
+                                         this._onWindowDemandsAttention.bind(this));
+        this._windowMarkedUrgentId = global.display.connect('window-marked-urgent',
+                                         this._onWindowDemandsAttention.bind(this));
+    },
+
+    _onWindowDemandsAttention: function(display, window) {
+        if (!window || window.has_focus() || window.is_skip_taskbar())
+            return;
+
+        Main.activateWindow(window);
+    },
+
+    destroy: function() {
+        global.display.disconnect(this._windowDemandsAttentionId);
+        global.display.disconnect(this._windowMarkedUrgentId);
+        Main.windowAttentionHandler._windowDemandsAttentionId = global.display.connect('window-demands-attention',
+                         Main.windowAttentionHandler._onWindowDemandsAttention.bind(Main.windowAttentionHandler));
+        Main.windowAttentionHandler._windowMarkedUrgentId = global.display.connect('window-marked-urgent',
+                         Main.windowAttentionHandler._onWindowDemandsAttention.bind(Main.windowAttentionHandler));
+    }
 }
 
-StealMyFocus.prototype = {
-  _init : function() {
-    this._tracker = Shell.WindowTracker.get_default();
-    this._handlerid = global.display.connect('window-demands-attention', Lang.bind(this, this._onWindowDemandsAttention));
-  },
-
-  _onWindowDemandsAttention: function(display, window) {
-    Main.activateWindow(window);
-  },
-
-  destroy: function () {
-    global.display.disconnect(this._handlerid);
-  }
-}
-
-
-WindowIsReadyRemover.prototype = {
-
-  _init : function() {
-    this._tracker = Shell.WindowTracker.get_default();
-    log('Disabling Window Is Ready Notification')
-    global.display.disconnect(Main.windowAttentionHandler._windowDemandsAttentionId);
-  },
-
-  destroy: function () {
-    global.display.disconnect(this._handlerid);
-  }
-}
-
-let windowIsReadyRemover;
-let stealmyfocus;
+let dontannoy;
 
 function init() {
 }
 
 function enable() {
-  stealmyfocus = new StealMyFocus();
-  windowIsReadyRemover = new WindowIsReadyRemover();
+    dontannoy = new DontAnnoy();
 }
 
 function disable() {
-  stealmyfocus.destroy();
-  windowIsReadyRemover.destroy();
+    dontannoy.destroy();
 }
